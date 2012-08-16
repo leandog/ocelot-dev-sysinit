7033efc228639a268da13c198468fb32ef926f8c
Eric Hankinson <eric.hankinson@leandog.com>
2012-08-16 11:40:26 -0400

    Changing way script is called

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ ocelot-dev-sysinit
 ==================
 
 Some initial configuration for new Ocelot Team Members
+
+
+To install, type this into a terminal session:
+
+bash <(curl -fsSkL rar.github.com/leandog/ocelot-dev-sysinit/master.install.sh)
+
--- install.sh
+++ install.sh
@@ -20,12 +20,12 @@ if [[ $result =~ No\ such\ file ]]
 then
   echo "Looks like you don't have a .bash_profile, let's make one!"
   touch ~/.bash_profile
-  `cat >> ~/.bash_profile <<EOL
+  cat >> ~/.bash_profile <<EOL
   if [ -f ~/.bashrc ] && [ "${SHELL##*/}" == "bash" ]
   then
     . ~/.bashrc
   fi
-  EOL`
+  EOL
 fi
 
 result=`which brew 2>&1`
