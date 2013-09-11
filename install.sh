90d6f2788a2d2f97878fd358e20cd05e8ae233c1
Eric Hankinson <eric.hankinson@gmail.com>
2013-09-11 10:05:23 -0400

    Typos and some additional echo clarity

--- install.sh
+++ install.sh
@@ -5,14 +5,14 @@ then
   echo -e "\nIt looks like you don't have Xcode installed. You need Xcode 4.3 or higher with the Command Line Tools installed to be able to continue"
   exit
 else
-  echo -e "\nYou have `xcodebuild -version` installed"
+  echo -e "\n**** You have `xcodebuild -version` installed"
 fi
 
 result=`which gcc`
 
 if [[ -z "$result" ]]
 then
-  echo -e "\nIt looks like the Command Line Tools aren't installed. If you have Xcode 4.3 or higher installed, launch Xcode, open the preferences, click on Downloads, and be sure to install the Command Line Tools!\n\nAfter you have installed the Command Line Tools, rerun this script."
+  echo -e "\n**** It looks like the Command Line Tools aren't installed. If you have Xcode 4.3 or higher installed, launch Xcode, open the preferences, click on Downloads, and be sure to install the Command Line Tools!\n\nAfter you have installed the Command Line Tools, rerun this script."
   exit
 fi
 
@@ -20,15 +20,15 @@ result=`which brew 2>&1`
 
 if [[ -z "$result" ]]
 then
-  echo -e "\nHomeBrew is not installed"
-  echo "Installing HomeBrew"
+  echo -e "\n**** HomeBrew is not installed"
+  echo "**** Installing HomeBrew"
   ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
   echo "export PATH=/usr/local/bin:/usr/local/sbin:$PATH" >> ~/.bashrc
   source ~/.bashrc
   brew tap homebrew/dupes
 else
-  echo -e "\nHomeBrew is already installed: `echo $result`"
-  echo "Upgrading HomeBrew to the latest build"
+  echo -e "\n**** HomeBrew is already installed: `echo $result`"
+  echo "**** Upgrading HomeBrew to the latest build"
   brew update
   brew tap homebrew/dupes
 fi
@@ -37,13 +37,13 @@ result=`brew list`
 
 if ! [[ $result =~ ack ]]
 then
-  echo -e "\nInstalling Ack, this will help with Vim later"
+  echo -e "\n**** Installing Ack, this will help with Vim later"
   brew install ack
 fi
 
 if ! [[ $result =~ the_silver_searcher ]]
 then
-  echo -e "\nInstalling the_silver_searcher, this will help with Vim later"
+  echo -e "\n**** Installing the_silver_searcher, this will help with Vim later"
   brew install the_silver_searcher
 fi
 
@@ -51,7 +51,7 @@ othergit=`which git`
 
 if ! [[ $result =~ git || -z "$othergit" ]]
 then
-  echo -e "\nInstalling git, you'll need this for source control"
+  echo -e "\n**** Installing git, you'll need this for source control"
   brew install git
 
   cat >> ~/.gitconfig <<EOL
@@ -101,22 +101,22 @@ fi
 
 if ! [[ $result =~ apple-gcc42 ]]
 then
-  echo -e "\nInstalling Apple's older GCC 4.2 binaries from HomeBrew to be able to build Ruby prior to 1.9.3 and most gems"
+  echo -e "\n**** Installing Apple's older GCC 4.2 binaries from HomeBrew to be able to build Ruby prior to 1.9.3 and most gems"
   brew install apple-gcc42
-  echo -e "\nSymlinking GCC in"
+  echo -e "\n**** Symlinking GCC in"
   #sudo ln -s /usr/local/bin/gcc-4.2 /usr/bin/gcc-4.2
 fi
 
 
 if ! [[ $result =~ vim ]]; then
-  echo -e "\nInstalling vim"
+  echo -e "\n**** Installing vim"
   brew install vim
 
   source ~/.bashrc
-  echo -e "\nIntalling Menlo Regular for Powerline font which allows unicode characters to display"
+  echo -e "\n**** Installing Menlo Regular for Powerline font which allows unicode characters to display"
   curl -so /Library/Fonts/Menlo\ Regular\ For\ Powerline.ttf https://raw.github.com/Lokaltog/powerline-fonts/master/Menlo/Menlo%20Regular%20for%20Powerline.ttf
 
-  echo -e "Currently will overwrite your .vim rc"
+  echo -e "**** Currently will overwrite your .vim rc"
   git clone https://github.com/leandog/vim-config.git ~/.vim
   ~/.vim/postinstall.sh
 fi
@@ -125,8 +125,8 @@ result=`which rvm`
 
 if [[ -z "$result" ]]
 then
-  echo -e "\nRVM Not installed"
-  echo "Installing RVM"
+  echo -e "\n**** RVM Not installed"
+  echo "**** Installing RVM"
   touch ~/.bashrc
   curl -L https://get.rvm.io | bash -s stable
   curl -so ~/.rvmsh https://raw.github.com/gist/898797/ead7ee759f8a1445db781b5b15bda49b418311f4//etc/profile.d/rvm.sh
@@ -135,9 +135,9 @@ then
 source ~/.rvmsh
 EOL
 
-  echo -e "\nInstalling a default .gemrc"
+  echo -e "\n**** Installing a default .gemrc"
   echo "gem: --no-ri --no-rdoc" >> ~/.gemrc
-  echo -e "\nInstalling a default .rvmrc"
+  echo -e "\n**** Installing a default .rvmrc"
   cat > ~/.rvmrc <<EOL
 rvm_ps1=1
 rvm_path="${HOME}/.rvm"
@@ -148,8 +148,8 @@ EOL
   source ~/.bashrc
   rvm reload
 else
-  echo -e "\nRVM installed: `echo $result`"
-  echo "Upgrading RVM to the latest build"
+  echo -e "\n**** RVM installed: `echo $result`"
+  echo "**** Upgrading RVM to the latest build"
   rvm get head
   rvm reload
 fi
@@ -157,7 +157,7 @@ fi
 if ! [[ -f ~/.bash_profile ]]
 then
 
-  echo -e "\nLooks like you don't have a .bash_profile, let's make one!"
+  echo -e "\n**** Looks like you don't have a .bash_profile, let's make one!"
   touch ~/.bash_profile
 
   cat > ~/.bash_profile <<EOL
